34724b1f58d7846f8e6bbfb4112a61b3e124c29a
IonutBocos <101240913+IonutBocos@users.noreply.github.com>
2022-07-08 09:48:31 +0300

    Exercitii clasa

--- M01S01/ex03/app.js
+++ M01S01/ex03/app.js
@@ -1,5 +1,6 @@
 var inputLength = document.getElementById('length');
 var inputWidth = document.getElementById('width');
+var inputHeight = document.getElementById('height');
 var elementResult = document.getElementById('result');
 var form = document.querySelector('form');
 
@@ -7,9 +8,10 @@ form.addEventListener('submit', function (event) {
   event.preventDefault();
   var length = inputLength.value || 0;
   var width = inputWidth.value || 0;
+  var height = inputHeight.value || 0;
   var result = 0;
 
-  result = length * width;
+  result = length * width * height;
 
   elementResult.innerText = result;
 });
