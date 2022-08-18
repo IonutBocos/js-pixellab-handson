8b558d0268d1c2118a993f9e3c39b5c3b57e02b7
IonutBocos <101240913+IonutBocos@users.noreply.github.com>
2022-08-18 09:25:23 +0300

    Exercitii clasa 17.08

---
+++ M02S01/ex01/app.js
@@ -0,0 +1,10 @@
+class Car {
+  constructor(make, color, wheels, speed) {
+    this.make = make;
+    this.color = color;
+    this.wheels = wheels;
+    this.speed = speed;
+  }
+}
+
+const audi = new Car('Audi', 'black', 4, 50);
